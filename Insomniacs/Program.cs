e37b6fb7c89db3df68ef3e64de275b79a210ebe2
riyorax <maximiliansulistiyo@gmail.com>
2024-05-25 03:28:02 +0700

    feat : added regex generator and sql file 20 hours before deadline

--- Insomniacs/Program.cs
+++ Insomniacs/Program.cs
@@ -6,56 +6,15 @@ using System.IO;
 
 public class Program
 {
-    public static void Main()
+    static void Main()
     {
+        RegexAlayPattern regexAlay = new RegexAlayPattern();
+        regexAlay.GeneratePattern("Demetrius Demarcus Bartholomew James The Third");
 
-        String filePath = "../test/8__M_Left_ring_finger.BMP";
-        String filePath2 = "damn.bmp";
-        // Image<Rgba32> img = BMPToBytes.ConvertToBlackAndWhite(filePath2);
-
-        // process an image to ascii for each row
-
-        // example of comparing same text
-        // process bmp 
-        List<string> text = BMPToBytes.ConvertBMPToASCII(filePath);
-
-        // getting pattern
-        string pattern = BMPToBytes.getImagePattern(filePath2);
-
-
-        // trying to find pattern in each row of said text
-        int row = 0;
-        int i = -1;
-        foreach (string entries in text)
-        {
-            i = KMPAlgorithm.KMPSearch(pattern, entries);
-            if (i != -1)
-            {
-                break;
-            }
-            row++;
-
-        }
-        if (i != -1)
-        {
-            Console.WriteLine("Find at row : " + row + " at index " + i);
-            Console.WriteLine("ROW : " + text[row]);
-        }
-        else
-        {
-            Console.WriteLine("Not exact match !");
-        }
-
-
-
-        // foreach (string entry in text){
-        //     Console.WriteLine(entry);
-        // }
-        // string t1 = "ABCLOVELIFEAD";
-        // string t2 = "LOVE";
-        // int  i = KMPAlgorithm.KMPSearch(t2, t1);
-        // Console.WriteLine(i);
-        // BMPToBytes.getImagePattern(filePath2);
-
+        Console.WriteLine("Regex Pattern: " + regexAlay.GetPattern());
+        Console.WriteLine(regexAlay.IsMatch("demetrI5 d3M4rC5 brThlm3w Jms tH thiRd")); // True
+        Console.WriteLine(regexAlay.IsMatch("DM3tr1s Dmrcs b4rthLmw Jm5 th thrd")); // True
+        Console.WriteLine(regexAlay.IsMatch("dmtr5 Dm4rCUs b4rThLm3w jm5 tH THrd")); // True
+        Console.WriteLine(regexAlay.IsMatch("dmtr1U5 dmrcuS brthlm3w j4m5 Th thrd")); // True
     }
 }
