b38e904b8e28563a66762da14539bad309152fc9
Marvel Pangondian <marvel.pangondian01@gmail.com>
2024-05-31 11:24:22 +0700

    feat: experiment branch

---
+++ Insomniacs/Program.cs
@@ -0,0 +1,36 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+
+
+
+        string test = "kajsnd.aladlka.lsfdm.ladsdfasdfnmdfssakjdLKASJDLKAsd";
+        string test2 = "asdkjhdsfglsdfsdfasdfnmdfsasdasdasdadsf";
+        string pattern = "sdfasdfnmdfs";
+        BoyerMoore bm = new BoyerMoore(pattern);
+        KMPAlgorithm kmp = new KMPAlgorithm(pattern);
+
+        int result = bm.Search(test);
+        int result2 = bm.Search(test2);
+        int result3 = kmp.Search(test);
+        int result4 = kmp.Search(test2);
+        if (result != -1)
+        {
+            Console.WriteLine($"Pattern found at index {result}");
+            Console.WriteLine($"Pattern found at index {result2}");
+            Console.WriteLine($"Pattern found at index {result3}");
+            Console.WriteLine($"Pattern found at index {result4}");
+        }
+        else
+        {
+            Console.WriteLine("Pattern not found");
+        }
+    }
+}
