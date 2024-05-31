6f383afbbab5c64fbc4b0ef62896c1a3b5060b86
MarvelPangondian <marvel.pangondian01@gmail.com>
2024-05-31 13:58:47 +0700

    clean up

--- Insomniacs/Program.cs
+++ Insomniacs/Program.cs
@@ -9,16 +9,28 @@ public class Program
     public static void Main()
     {
         const string BASEDIR = "../";
-        string testSource = "test/83__M_Right_middle_finger.BMP";
+        // string testSource = "test/83__M_Right_middle_finger.BMP";
+        string testSource = "test/8__M_Right_middle_finger.BMP";
         Image<Rgba32> image  = BMPToBytes.ConvertToBlackAndWhite(BASEDIR + testSource);
         List<string> imageInBinary = BMPToBytes.ConvertImageToBinary(image);
+        List<String> asciiForm = BMPToBytes.ConvertBMPToASCII( BASEDIR + testSource);
         Console.WriteLine("83__M_Right_middle_finger: ");
         foreach (string s in imageInBinary ){
             Console.WriteLine(s);
         }
-        Console.WriteLine("Image pattern picked");
+        
         string imagePattern = BMPToBytes.GetImagePattern(BASEDIR + testSource);
+        Console.WriteLine("Image pattern picked");
+        Console.WriteLine(imagePattern);
 
         Console.WriteLine(imagePattern.Length);
+
+
+        Console.WriteLine("ASCII FORM :");
+        foreach(string s in asciiForm){
+            Console.WriteLine(s);
+        }
     }
 }
+
+
