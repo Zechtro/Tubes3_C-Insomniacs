340ecda15f64d7c43750e22c3a5ab6b3228d2e1b
MarvelPangondian <marvel.pangondian01@gmail.com>
2024-05-31 17:20:36 +0700

    regex tester

--- Insomniacs/Program.cs
+++ Insomniacs/Program.cs
@@ -8,28 +8,37 @@ public class Program
 {
     public static void Main()
     {
-        const string BASEDIR = "../";
-        string testSource = "test/83__M_Right_middle_finger.BMP";
-        // string testSource = "test/8__M_Right_middle_finger.BMP";
-        Image<Rgba32> image  = BMPToBytes.ConvertToBlackAndWhite(BASEDIR + testSource);
-        List<string> imageInBinary = BMPToBytes.ConvertImageToBinary(image);
-        List<String> asciiForm = BMPToBytes.ConvertBMPToASCII( BASEDIR + testSource);
-        Console.WriteLine("83__M_Right_middle_finger: ");
-        foreach (string s in imageInBinary ){
-            Console.WriteLine(s);
-        }
-        
-        string imagePattern = BMPToBytes.GetImagePattern(BASEDIR + testSource);
-        Console.WriteLine("Image pattern picked");
-        Console.WriteLine(imagePattern);
+        // const string BASEDIR = "../";
+        // string testSource = "test/83__M_Right_middle_finger.BMP";
+        // // string testSource = "test/8__M_Right_middle_finger.BMP";
+        // Image<Rgba32> image  = BMPToBytes.ConvertToBlackAndWhite(BASEDIR + testSource);
+        // List<string> imageInBinary = BMPToBytes.ConvertImageToBinary(image);
+        // List<String> asciiForm = BMPToBytes.ConvertBMPToASCII( BASEDIR + testSource);
+        // Console.WriteLine("83__M_Right_middle_finger: ");
+        // foreach (string s in imageInBinary ){
+        //     Console.WriteLine(s);
+        // }
 
-        Console.WriteLine(imagePattern.Length);
+        // string imagePattern = BMPToBytes.GetImagePattern(BASEDIR + testSource);
+        // Console.WriteLine("Image pattern picked");
+        // Console.WriteLine(imagePattern);
+
+        // Console.WriteLine(imagePattern.Length);
 
 
-        Console.WriteLine("ASCII FORM :");
-        foreach(string s in asciiForm){
-            Console.WriteLine(s);
-        }
+        // Console.WriteLine("ASCII FORM :");
+        // foreach(string s in asciiForm){
+        //     Console.WriteLine(s);
+        // }
+
+        RegexAlayPattern regexAlay = new RegexAlayPattern();
+        string baseString = "Marvel Pangondian";
+        regexAlay.GeneratePattern(baseString);
+        string input = "M4Rvl p4N6Ondn";
+
+        Console.WriteLine("Pattern: " + regexAlay.GetPattern());
+        Console.WriteLine("Is Match: " + regexAlay.IsMatch(input));
+        Console.WriteLine("Normalized: " + regexAlay.Normalize(input));
     }
 }
 
---
+++ Insomniacs/RegexAlayPattern.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class RegexAlayPattern
+{
+    private Dictionary<char, string> leetMap;
+    private Dictionary<char, char> reverseLeetMap;
+    private string regexPattern;
+
+    public RegexAlayPattern()
+    {
+        leetMap = new Dictionary<char, string>
+        {
+            { 'a', "4@" }, { 'e', "3" }, { 'i', "1!|" }, { 'o', "0" }, { 'u', "" },
+            { 's', "$5" }, { 't', "7+" }, { 'l', "1|" }, { 'z', "2" }, { 'g', "6" }
+        };
+        reverseLeetMap = new Dictionary<char, char>
+        {
+            { '4', 'a' }, { '@', 'a' }, { '3', 'e' }, { '1', 'i' }, { '!', 'i' },
+            { '|', 'i' }, { '0', 'o' }, { '$', 's' }, { '5', 's' }, { '7', 't' },
+            { '+', 't' }, { '2', 'z' }, { '6', 'g' }
+        };
+        regexPattern = "";
+    }
+
+    public void GeneratePattern(string baseString)
+    {
+        StringBuilder patternBuilder = new StringBuilder();
+        foreach (char c in baseString.ToLower())
+        {
+            if (leetMap.ContainsKey(c))
+            {
+                if ("aeiou".IndexOf(c) >= 0)
+                {
+                    patternBuilder.Append('[');
+                    patternBuilder.Append(c);
+                    foreach (char leetChar in leetMap[c])
+                    {
+                        patternBuilder.Append(leetChar);
+                    }
+                    patternBuilder.Append("]?");
+                }
+                else
+                {
+                    patternBuilder.Append('[');
+                    patternBuilder.Append(c);
+                    foreach (char leetChar in leetMap[c])
+                    {
+                        patternBuilder.Append(leetChar);
+                    }
+                    patternBuilder.Append(']');
+                }
+            }
+            else
+            {
+                patternBuilder.Append(Regex.Escape(c.ToString()));
+            }
+        }
+        regexPattern = patternBuilder.ToString();
+    }
+
+    public bool IsMatch(string input)
+    {
+        return Regex.IsMatch(input.ToLower(), regexPattern);
+    }
+
+    public string GetPattern()
+    {
+        return regexPattern;
+    }
+
+    public string Normalize(string input)
+    {
+        StringBuilder normalizedText = new StringBuilder();
+        foreach (char c in input)
+        {
+            if (reverseLeetMap.ContainsKey(c))
+            {
+                normalizedText.Append(reverseLeetMap[c]);
+            }
+            else
+            {
+                normalizedText.Append(c);
+            }
+        }
+        return normalizedText.ToString();
+    }
+}
