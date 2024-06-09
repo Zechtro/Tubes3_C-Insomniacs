e1d51dea4d519ed73259d315ff820df94dafe187
Marvel Pangondian <marvel.pangondian01@gmail.com>
2024-06-09 11:01:45 +0700

    remove old versions of functions

--- src/project/backend/BMPtoBytes.cs
+++ src/project/backend/BMPtoBytes.cs
@@ -9,7 +9,7 @@ public class BMPToBytes
     public static SixLabors.ImageSharp.Image<Rgba32> ConvertToBlackAndWhite(string filePath)
     {
         SixLabors.ImageSharp.Image<Rgba32> image;
-        
+
         // load filePath
         image = SixLabors.ImageSharp.Image.Load<Rgba32>(filePath);
 
@@ -43,65 +43,6 @@ public class BMPToBytes
         }
         binaryRows = RemoveTrailingZeroRows(binaryRows);
         return binaryRows;
-    }
-
-    public static List<string> ConvertBMPToASCIIOldVersion(string filePath)
-    {
-        var image = ConvertToBlackAndWhite(filePath);
-        List<string> asciiRows = new List<string>();
-        int width = image.Width;
-        int height = image.Height;
-
-        for (int y = 0; y < height; y++)
-        {
-            StringBuilder rowAscii = new StringBuilder();
-            StringBuilder binaryString = new StringBuilder();
-            for (int x = 0; x < width; x++)
-            {
-                Rgba32 pixel = image[x, y];
-                bool isWhite = pixel.R > 128 && pixel.G > 128 && pixel.B > 128;
-
-                char binaryChar = isWhite ? '1' : '0'; // Using '1' for white and '0' for black
-                binaryString.Append(binaryChar);
-
-                // Group binary bits into bytes (8 bits)
-                if (binaryString.Length == 8)
-                {
-                    byte b = Convert.ToByte(binaryString.ToString(), 2);
-                    rowAscii.Append((char)b);
-                    binaryString.Clear();
-                }
-            }
-
-            // Handle any remaining bits (less than 8 bits)
-            if (binaryString.Length > 0)
-            {
-                // Pad with zeros if binary length is less than 8
-                while (binaryString.Length < 8)
-                {
-                    binaryString.Append('0');
-                }
-                byte b = Convert.ToByte(binaryString.ToString(), 2);
-                rowAscii.Append((char)b);
-            }
-
-            asciiRows.Add(rowAscii.ToString());
-        }
-
-        // remove trailing zero
-        for (int i = asciiRows.Count - 1; i >= 0; i--)
-        {
-            if (asciiRows[i].All(c => c == '\0'))
-            {
-                asciiRows.RemoveAt(i);
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return asciiRows;
     }
 
     public static List<string> ConvertBMPtoASCII(string filePath)
@@ -140,98 +81,6 @@ public class BMPToBytes
             asciiRows.Add(rowAscii.ToString());
         }
         return asciiRows;
-    }
-
-    public static string GetImagePatternOldVersion(string filename)
-    {
-        Console.WriteLine();
-        var blackAndWhiteBMP = ConvertToBlackAndWhite(filename);
-        List<string> binaryRows = ConvertImageToBinary(blackAndWhiteBMP);
-
-        // picking top, center, and bottom pattern
-        // pick all possible rows
-        string pickedPattern = "";
-        int pickedHomogenity = int.MaxValue;
-        string pickedPatternInBinary = "";
-
-        int pickedPatternRow = -1;
-        string pattern;
-        List<string> segments;
-
-        int[] allRow = new int[3];
-        for (int i = 0; i < 3; i++)
-        {
-            allRow[i] = (int)Math.Floor(binaryRows.Count * ((i + 1) / 4.0));
-        }
-
-        for (int patternIndex = 0; patternIndex < 3; patternIndex++)
-        {
-            pattern = binaryRows[allRow[patternIndex]];
-            // TODO: find a better way to handle non length of multiple 8
-            // Pad the pattern to make its length a multiple of 8
-            int paddingLength = 8 - (pattern.Length % 8);
-            if (paddingLength != 8)
-            {
-                pattern = pattern.PadRight(pattern.Length + paddingLength, '0');
-            }
-            segments = new List<string>();
-
-            // Separete binary pattern into segments of 8 bits
-            for (int j = 0; j < pattern.Length; j += 8)
-            {
-                string segment = pattern.Substring(j, 8);
-                segments.Add(segment);
-            }
-
-            // TODO: determine if 64 bit pattern is better than 32 bit
-            // Determine the 32 bits in the center of the row
-            int minHomogeneity = int.MaxValue;
-            int startIndex = 0;
-
-            for (int z = 0; z <= segments.Count - 4; z++)
-            {
-                int homogeneity = 0;
-                // Count the homogeneity in the current sequence of 4 segments
-                for (int j = z; j < z + 4; j++)
-                {
-                    int zeroCount = segments[j].Count(c => c == '0');
-                    int oneCount = 8 - zeroCount;
-                    homogeneity += Math.Abs(zeroCount - oneCount);
-                }
-                // Update the minimum homogeneity and the starting index if necessary
-                if (homogeneity < minHomogeneity)
-                {
-                    minHomogeneity = homogeneity;
-                    startIndex = z;
-                }
-            }
-
-            if (minHomogeneity < pickedHomogenity)
-            {
-                pickedPatternInBinary = "";
-                List<string> result = segments.GetRange(startIndex, 4);
-                StringBuilder finalPattern = new StringBuilder();
-                foreach (string entry in result)
-                {
-                    pickedPatternInBinary += entry;
-                    int asciiValue = Convert.ToInt32(entry, 2);
-                    char asciiCharacter = (char)asciiValue;
-                    finalPattern.Append(asciiCharacter);
-                }
-                pickedPattern = finalPattern.ToString();
-                pickedHomogenity = minHomogeneity;
-                pickedPatternRow = allRow[patternIndex];
-
-            }
-        }
-
-
-        if (string.IsNullOrEmpty(pickedPattern))
-        {
-            throw new Exception("Error in coverting fingerprint to a pattern");
-        }
-
-        return pickedPattern;
     }
 
     public static string GetImagePattern(string filename)  
